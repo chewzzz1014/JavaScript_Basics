5dbdb26a7442bd547c204e07d7016da8a3c09b7e
chewzzz <chewziqing@gmail.com>
2022-03-05 18:52:05 +0800

    Added few more examples

--- number.js
+++ number.js
@@ -2,6 +2,11 @@
 var a = .89;
 var b = 98.;
 
+console.log(`a/b: ${a/b}`)
+console.log(1/0) //Infinity
+console.log("Wow"/2) //NaN
+console.log( NaN *3) //NaN
+
 //operation on number
 console.log(b.toFixed(4)); //98.0000
 console.log(b.toPrecision(4)); //98.00
