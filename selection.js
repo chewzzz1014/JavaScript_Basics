9589c9579a856b6a1a5e581dde5e1c89fa602681
chewzzz <chewziqing@gmail.com>
2022-03-06 15:30:38 +0800

    Initial Commit

---
+++ selection.js
@@ -0,0 +1,18 @@
+//falsy is always false
+falsy = [0, "", null, undefined, NaN];
+
+
+falsy.forEach( function(value){ (value) ? console.log("It's truthy!") : console.log("It's falsy!");} );
+
+
+
+
+let grade = prompt("Enter GPA:",0);
+
+let classScored = ( grade<=4.0 && grade>=3.6)? "First":
+            ( grade<=3.5 && grade>=3.0)? "Second Class Upper":
+            ( grade<=2.9 && grade>=2.5)? "Second Class Lower":
+            ( grade<=2.4 && grade>=2.0)? "Third":
+            ( grade<=1.9 && grade>=1.0)? "Pass": "Fail";
+
+alert(`You got ${classScored}`);
