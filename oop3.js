1b6efbb84ce185fdcfe1c09a15fe9448f897dfdb
chewzzz <chewziqing@gmail.com>
2022-03-08 18:40:57 +0800

    Added more examples

--- oop3.js
+++ oop3.js
@@ -23,3 +23,63 @@ function Person(name, age){
 //let name = prompt("Enter name:");
 //let age = prompt("Enter age:");
 let user = new Person(prompt("Enter name:"), prompt("Enter age:"));
+
+
+
+//////////////////////////////////////////////////////////////////////////////
+
+//constructor for Calculator
+
+function Calculator(){
+  this.read = function(){
+    this.a = +prompt("Enter value 1"); //make sure it's number
+    this.b = +prompt("Enter value 2");
+  }
+
+  this.sum = function(){
+    alert("Their sum is "+ (this.a + this.b));
+  }
+
+  this.mul = function(){
+    alert("Their result of multiplication is "+ (this.a * this.b));
+  }
+}
+
+let calculator = new Calculator();
+calculator.read();
+calculator.sum();
+calculator.mul();
+
+
+/////////////////////////////////////////////////////////////////
+//calculate the sum of all values entered by user (stored in variable value). Able to acces the initial value
+
+function Accumulator(num){
+  this.startingValue = num;
+  this.value = num;
+  this.count = 1;
+
+  this.read = function(){
+    let number = +prompt("Enter number");
+    this.value += number;
+    this.count++;
+  };
+
+  this.printInitial = function(){
+    alert("We start with "+this.startingValue+"!");
+  };
+
+  this.printSum = function(){
+    alert("Total values of "+this.count+" entered is "+this.value);
+  };
+
+}
+
+let accumulator = new Accumulator(1);
+accumulator.read();
+accumulator.read();
+accumulator.read();
+accumulator.read();
+accumulator.read();
+accumulator.printInitial();
+accumulator.printSum();
