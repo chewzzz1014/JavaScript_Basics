1fe99051185caec4d0650126d7607354ba39ba63
chewzzz <chewziqing@gmail.com>
2022-07-28 18:19:04 +0800

    Added more examples

--- general-syntax/nullishCoalescingOperator.js
+++ general-syntax/nullishCoalescingOperator.js
@@ -3,5 +3,13 @@ a ?? b
 //is equivalent to
 (a!== null && a!==undefined)? a:b
 
+// if userName is not provided, then it's "Anonymous"
 let userName = prompt("Enter user name:");
 alert("Welcome, "+ (userName ?? "Anonymous" )+"!");
+
+
+// for assigning default value if the variable is null/undefined
+let height = +prompt("Enter height",null);
+let width = +prompt("Enter width", null);
+let area = (height ?? 100) * (width ?? 50);
+alert(area);
