23c51bf46956fe4fcd02ba989030fc8abda63d48
chewzzz <chewziqing@gmail.com>
2022-07-28 14:35:25 +0800

    Initial Commit

---
+++ module/package-management.js
@@ -0,0 +1,38 @@
+"use strict"
+
+/*
+  Package/Dependencies/Modules: Bundle of code and/or assets
+  - libraries
+  - tools
+  - shared components
+  - our application
+
+  - NPM: software registry
+  - also CLI bundled with every node.js installation
+*/
+
+
+/*
+package.json: every project's starting point
+- consists of:
+     - metedata: project's name, version description, author, repository, license, etc
+     - dependencies: list of packages used by project
+     - scripts: commands to automate development tasks*/
+
+
+/*
+1. Download a package and add to dependencies
+    npm install <package_name>
+
+2. Download and add package to devDependencies (package installed for development only)
+      npm install --save-dev <package_name>
+*/
+
+/*
+Create a package:
+
+  1. In bash, enter "npm init -y". package.json will be create automatically.
+  2. Install "prettier" package in devDependencies. In bash, enter "npm install--save-dev prettier"
+  3. In package.json, "scripts"'s values will hold the command we want. To execute these commands, in bash, enter "npm run <script_name>"
+  4.
+*/
