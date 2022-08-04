44e9ee4bcc53097f779856f267cca7150b0e4396
chewzzz <chewziqing@gmail.com>
2022-08-04 14:30:02 +0800

    Add function ecpression examples

--- function/function-basics.js
+++ function/function-basics.js
@@ -10,7 +10,7 @@ function f(){
 f("wee"); // it's ok to pass argument to no-args function
 console.log(typeof f);
 
-// f1 did not return anything. The return value is undefined 
+// f1 did not return anything. The return value is undefined
 function f1(name){
   console.log(`Nice to meet you, ${name}!`);
 }
@@ -18,23 +18,29 @@ f1("chewzzz");
 f1();   // Nice to meet you, undefined!
 
 
-
-
-// arrow function
-// will always return something
-// must be assigned to a variable or immediately used
-const addition = (a,b) => a+b;
-const subtraction = (a, b) => a-b;
-const mul = (a,b) => a*b;
-const div = (a,b) => a%b==0;
-const printHello = () => console.log("Hello");
-const div2 = (a) => {
-  if (a%2==0)
-    return "Divisible by 2";
-  else
-    return "Not Divisible by 2";
+// function expression: function is a value
+// not hoisted
+let greeting = function(name="Guest"){
+  console.log(`Nice to meet you, ${name}!`);
 }
 
-console.log(`Is 346 divisible by 6?: ${div(346, 6)}`);
-console.log(`0 is ${div2(0)}`);
-printHello();
+console.log(greeting);  // print the function code
+greeting("chewzzz");
+
+
+// passing function as argument
+function ask(q, yes, no){
+  if (confirm(q)) yes();
+  else no();
+}
+
+function agree(){
+  alert("You agreed");
+}
+
+function disagree(){
+  alert("You disagreed");
+}
+
+// no parentheses when passing function code as argument 
+ask("Please donate", agree, disagree);
