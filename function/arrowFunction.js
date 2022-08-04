50708978af37dd8a59bdcf222a263dd71996f15e
chewzzz <chewziqing@gmail.com>
2022-08-04 14:30:24 +0800

    Add more examples

--- function/arrowFunction.js
+++ function/arrowFunction.js
@@ -27,3 +27,23 @@ askUser(
   ()=> {alert("You agreed.")},
   ()=> {alert("You disagreed.")}
 );
+
+
+// arrow function
+// will always return something
+// must be assigned to a variable or immediately used
+const addition = (a,b) => a+b;
+const subtraction = (a, b) => a-b;
+const mul = (a,b) => a*b;
+const div = (a,b) => a%b==0;
+const printHello = () => console.log("Hello");
+const div2 = (a) => {
+  if (a%2==0)
+    return "Divisible by 2";
+  else
+    return "Not Divisible by 2";
+}
+
+console.log(`Is 346 divisible by 6?: ${div(346, 6)}`);
+console.log(`0 is ${div2(0)}`);
+printHello();
