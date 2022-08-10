d786dcf4b85be8ce4e77905f81b13c2250523b75
chewzzz <chewziqing@gmail.com>
2022-08-10 11:23:28 +0800

    Add more comments

--- object_to_primitive.js
+++ object_to_primitive.js
@@ -1,5 +1,13 @@
 // convert object to primitive type (number/String)
 
+/*
+Conversion Rule
+1. No conversion to boolean. All objects are true in a boolean context.
+2. Numeric conversion happens when substract object/apply mathematical functions (eg,date object)
+3. String conversion: when output an object with alert(obj) and in similar context.
+*/
+
+
 /* syntax: obj[Symbol.toPrimitive] = function (hint)
  * {      if (hint=="String")
  *              //do something
@@ -14,10 +22,12 @@
    name: "chewzzz",   //string
    age: 20,   //number
 
+   // convert object "person" to string or numeric
    [Symbol.toPrimitive](hint){
      return (hint=="string")? this.name: this.age;
    },   //hanging comma
  };
+
 
  alert(person);   //String
  alert(+person);    //number
