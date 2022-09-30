ed7899be1f9885a66e86bcc1c1d839c904afb9bf
chewzzz <chewziqing@gmail.com>
2022-09-30 14:34:12 +0800

    Add view comment by id and assign unique comment id

--- express/REST_Demo/index.js
+++ express/REST_Demo/index.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const path = require("path")
 const app = express()
+const { v4: uuid } = require("uuid");
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -10,22 +12,27 @@ app.set("views", path.join(__dirname, "views"));
 
 const comments = [
     {
+        id: uuid(),
         username: "Todd",
         comment: "hahahahah so funny"
     },
     {
+        id: uuid(),
         username: "PikachuMon",
         comment: "woof woof woof"
     },
     {
+        id: uuid(),
         username: "makimaSlave",
         comment: "can't wait for the anime"
     },
     {
+        id: uuid(),
         username: "Quincy",
         comment: "are you serious?..."
     },
     {
+        id: uuid(),
         username: "lolololBoi",
         comment: "hmmmmm don't think it worths the time"
     }
@@ -44,10 +51,17 @@ app.get("/comments/new", (req, res) => {
 // POST request after submitted the form
 app.post("/comments", (req, res) => {
     const { username, comment } = req.body;
-    comments.push({ username, comment })
+    comments.push({ username, comment, id: uuid() })
     // redirect to comments
     res.redirect("/comments");
 })
+
+app.get("/comments/:id", (req, res) => {
+    const { id } = req.params;
+    const comment = comments.find(c => c.id === id);
+    res.render("comments/show", { comment });
+})
+
 
 app.get("/tacos", (req, res) => {
     res.send("GET /tacos response");
