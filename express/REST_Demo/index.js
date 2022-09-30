dd1fc3671ac2d092221b371c79e35704c6713c66
chewzzz <chewziqing@gmail.com>
2022-09-30 13:58:25 +0800

    Add redirect

--- express/REST_Demo/index.js
+++ express/REST_Demo/index.js
@@ -45,6 +45,8 @@ app.get("/comments/new", (req, res) => {
 app.post("/comments", (req, res) => {
     const { username, comment } = req.body;
     comments.push({ username, comment })
+    // redirect to comments
+    res.redirect("/comments");
 })
 
 app.get("/tacos", (req, res) => {
