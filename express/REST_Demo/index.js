c7ce5c1b7efde6993dcdf8894f84441d561db06b
chewzzz <chewziqing@gmail.com>
2022-09-30 13:48:32 +0800

    Add get and post request for comments

--- express/REST_Demo/index.js
+++ express/REST_Demo/index.js
@@ -8,6 +8,44 @@ app.use(express.json());
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+const comments = [
+    {
+        username: "Todd",
+        comment: "hahahahah so funny"
+    },
+    {
+        username: "PikachuMon",
+        comment: "woof woof woof"
+    },
+    {
+        username: "makimaSlave",
+        comment: "can't wait for the anime"
+    },
+    {
+        username: "Quincy",
+        comment: "are you serious?..."
+    },
+    {
+        username: "lolololBoi",
+        comment: "hmmmmm don't think it worths the time"
+    }
+];
+
+
+app.get("/comments", (req, res) => {
+    res.render("comments/index", { comments })
+})
+
+// display form
+app.get("/comments/new", (req, res) => {
+    res.render("comments/new");
+})
+
+// POST request after submitted the form
+app.post("/comments", (req, res) => {
+    const { username, comment } = req.body;
+    comments.push({ username, comment })
+})
 
 app.get("/tacos", (req, res) => {
     res.send("GET /tacos response");
