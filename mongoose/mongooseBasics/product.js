048d317e4d1152ce5c56a923836a589265b63366
chewzzz <chewziqing@gmail.com>
2022-10-05 14:16:38 +0800

    Add more schema validation

--- mongoose/mongooseBasics/product.js
+++ mongoose/mongooseBasics/product.js
@@ -14,11 +14,32 @@ mongoose.connect('mongodb://localhost:27017/shopApp', { useNewUrlParser: true })
 const productSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: true
+        required: true,
+        maxLength: 20
     },
     price: {
         type: Number,
-        required: true
+        required: true,
+        min: 0
+    },
+    onSale: {
+        type: Boolean,
+        default: false
+    },
+    categories: {
+        // a string of number
+        // boolean/number will be converted into string
+        type: [String]
+    },
+    qty: {
+        online: {
+            type: Number,
+            default: 0
+        },
+        inStore: {
+            type: Number,
+            default: 0
+        }
     }
 })
 
@@ -26,7 +47,7 @@ const productSchema = new mongoose.Schema({
 const Product = mongoose.model("Product", productSchema);
 
 // create instance of Product
-const bike = new Product({ name: "Mountain Bike", price: 500 });
+const bike = new Product({ name: "Bike Helmet", price: 19.50, categories: ["Cycling", "Safety"], qty: { online: 10, inStore: 15 } });
 
 // save 
 bike.save()
