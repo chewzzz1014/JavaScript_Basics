5ed95eb91671ab4fcc17ed3ef9167c8d08540526
chewzzz <chewziqing@gmail.com>
2022-10-05 15:00:49 +0800

    Add instance methods

--- mongoose/mongooseBasics/product.js
+++ mongoose/mongooseBasics/product.js
@@ -49,33 +49,60 @@ const productSchema = new mongoose.Schema({
     }
 })
 
+// add instance method
+productSchema.methods.greet = function () {
+    console.log("hiiiiiiiiiiiiiiiiiiiiiiiiiiii");
+    console.log(`- from ${this.name}`);
+}
+productSchema.methods.toggleOnSale = function () {
+    this.onSale = !this.onSale;
+    // promise-like thenable
+    return this.save();
+}
+productSchema.methods.addCategories = function (newCat) {
+    this.categories.push(newCat);
+    return this.save();
+}
+
 // create a model 
 const Product = mongoose.model("Product", productSchema);
 
+const findProduct = async () => {
+    const foundProduct = await Product.findOne({ name: "Bike Helmet" });
+    console.log(foundProduct);
+    foundProduct.greet();
+    await foundProduct.toggleOnSale();
+    console.log(foundProduct);
+    await foundProduct.addCategories("Outdoors");
+    console.log(foundProduct);
+}
+
+findProduct();
+
 // create instance of Product
-const bike = new Product({ name: "Cycling Jersey", price: 28.50, categories: ["Cycling"], size: "XS" });
+// const bike = new Product({ name: "Cycling Jersey", price: 28.50, categories: ["Cycling"], size: "XS" });
 
-// // save 
-bike.save()
-    .then(data => {
-        console.log("We got...");
-        console.log(data);
-    })
-    .catch(err => {
-        console.log("We got error!");
-        console.log(err);
-    })
+// // // save
+// bike.save()
+//     .then(data => {
+//         console.log("We got...");
+//         console.log(data);
+//     })
+//     .catch(err => {
+//         console.log("We got error!");
+//         console.log(err);
+//     })
 
 
-// new: true so that it show the updated data
-// by default, the inserted data will not follow schema validation. By setting "runValidators: true", the inserted value will follow the validation too.
-Product.findOneAndUpdate({ name: "Tire Pump" }, { price: -10.99 }, { new: true, runValidators: true })
-    .then(data => {
-        console.log("We got...");
-        console.log(data);
-    })
-    .catch(err => {
-        console.log("We got error!");
-        console.log(err);
-    })
+// // new: true so that it show the updated data
+// // by default, the inserted data will not follow schema validation. By setting "runValidators: true", the inserted value will follow the validation too.
+// Product.findOneAndUpdate({ name: "Tire Pump" }, { price: -10.99 }, { new: true, runValidators: true })
+//     .then(data => {
+//         console.log("We got...");
+//         console.log(data);
+//     })
+//     .catch(err => {
+//         console.log("We got error!");
+//         console.log(err);
+//     })
 
