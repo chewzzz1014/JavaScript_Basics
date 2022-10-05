ce54db920dc241e99903ecad9080dffb7547d230
chewzzz <chewziqing@gmail.com>
2022-10-05 13:33:47 +0800

    Add schema validation

--- mongoose/mongooseBasics/product.js
+++ mongoose/mongooseBasics/product.js
@@ -10,4 +10,32 @@ mongoose.connect('mongodb://localhost:27017/shopApp', { useNewUrlParser: true })
     })
 
 
+// Schema validation
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    }
+})
+
+// create a model 
+const Product = mongoose.model("Product", productSchema);
+
+// create instance of Product
+const bike = new Product({ name: "Mountain Bike", price: 500 });
+
+// save 
+bike.save()
+    .then(data => {
+        console.log("We got...");
+        console.log(data);
+    })
+    .catch(err => {
+        console.log("We got error!");
+        console.log(err);
+    })
 
