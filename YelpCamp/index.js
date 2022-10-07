a757a5087826a61fe5dfda6c2cbc65d32ed982db
chewzzz <chewziqing@gmail.com>
2022-10-07 14:10:22 +0800

    Add mongoose

--- YelpCamp/index.js
+++ YelpCamp/index.js
@@ -1,6 +1,8 @@
+// import
 const mongoose = require("mongoose");
 const express = require("express");
 const path = require("path");
+const Campground = require("./models/campground")
 const app = express();
 
 app.set("view engine", "ejs");
@@ -9,9 +11,28 @@ app.set("views", path.join(__dirname, "views"))
 app.use(express.urlencoded({ extended: true }))
 // app.use(express.static(path.join(__dirname, "public")))
 
+// set up mongodb
+mongoose.connect("mongodb://localhost:27017/yelp-camp", {
+    useNewUrlParser: true,
+    // useCreateIndex: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database Connected");
+});
+
+
 
 app.get("/", (req, res) => {
     res.render("home")
+})
+app.get("/makecampground", async (req, res) => {
+    const camp = new Campground({ title: "My Backyard" });
+    await camp.save();
+    res.send(camp);
 })
 
 app.listen(3000, () => {
