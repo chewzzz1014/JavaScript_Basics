a5889f793e3227486987a8c40d567ec8b35620c6
chewzzz <chewziqing@gmail.com>
2022-10-13 09:45:09 +0800

    Add image and desciption in mongo

--- YelpCamp/seeds/index.js
+++ YelpCamp/seeds/index.js
@@ -29,9 +29,13 @@ const seedDB = async () => {
     // generate 50 random campgrounds
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 30) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: "https://source.unsplash.com/collection/483251",
+            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel in ratione, sequi nisi laborum soluta tempore, omnis non rem modi eligendi earum, aliquam molestiae? Officiis quis quaerat aperiam iure fugiat?Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum facere iste vel at numquam ratione minus, sint quae quam aliquid pariatur optio nisi quidem possimus beatae quos voluptas dolorem officia.",
+            price: price
         })
         await camp.save();
     }
