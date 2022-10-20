b1228f8f6d8e8cc35dde95361d99c7f8861b6735
chewzzz <chewziqing@gmail.com>
2022-10-20 23:16:14 +0800

    Add regex grouping

--- regex/regex8.js
+++ regex/regex8.js
@@ -10,3 +10,16 @@ testRegex.test(testStr);
 // matching Eleanor Roosevelt, Franklin Roosevelt with or without middle name
 let myString = "Eleanor Roosevelt";
 let myRegex = /^(Franklin|Eleanor).*Roosevelt$/i;
+
+
+// capturing group: can be used for finding repeated substrings.
+// enclosing regex pattern to be capture inside ()
+//  substring matched by the group is saved to a temporary "variable", which can be accessed within the same regex using a backslash and the number of the capture group (e.g. \1). Capture groups are automatically numbered by the position of their opening parentheses (left to right), starting at 1.
+let repeatedStr = "row row row your boat";
+let repeatRegex = /(\w+) \1 \1/;
+repeatRegex.test(repeatStr); // Returns true
+repeatStr.match(repeatRegex); // Returns ["row row row", "row"]
+
+// catch same number that repeated thrice
+let repeatNum = "42 42 42";
+let reRegex = /^(\d+) \1 \1$/;
