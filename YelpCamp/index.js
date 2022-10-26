26224c257b13745368ad27c7118877db780c5123
chewzzz <chewziqing@gmail.com>
2022-10-26 17:41:10 +0800

    Add review into db

--- YelpCamp/index.js
+++ YelpCamp/index.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const express = require("express");
 const path = require("path");
 const Campground = require("./models/campground")
+const Review = require("./models/review");
 const methodOverride = require("method-override")
 const ejsMate = require("ejs-mate");
 const { nextTick } = require("process");
@@ -93,6 +94,15 @@ app.delete("/campgrounds/:id", catchAsync(async (req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect("/campgrounds");
+}))
+
+app.post("/campgrounds/:id/reviews", catchAsync(async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    const review = new Review(req.body.review);
+    campground.reviews.push(review);
+    await review.save();
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
 }))
 
 app.all("*", (req, res, next) => {
