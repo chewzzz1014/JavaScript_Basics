d9ae09d8b96bd23439447e977dbf1b62278ac99f
chewzzz <chewziqing@gmail.com>
2022-10-28 14:07:32 +0800

    Add express signed cookit

--- express/Cookies_Demo/index.js
+++ express/Cookies_Demo/index.js
@@ -2,7 +2,8 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const app = express();
 
-app.use(cookieParser());
+// secret used for signing cookie
+app.use(cookieParser("thisismysecret"));
 
 app.get("/greet", (req, res) => {
     // grab what's inside the cookie
@@ -17,7 +18,21 @@ app.get("/setname", (req, res) => {
     res.cookie("name", "chewzzz")
     res.cookie("animal", "dog")
 
-    res.send("OK!")
+    res.send("OK! Sent cookie.")
+})
+
+app.get("/getsignedcookie", (req, res) => {
+    // sign a cookie
+    res.cookie("fruit", "grape", { signed: true })
+    res.send("Sent signed cookie!")
+})
+
+app.get("/verifyfruit", (req, res) => {
+    // signed cookie is not shown
+    console.log(req.cookies);
+
+    // show signed cookie only
+    res.send(req.signedCookies)
 })
 
 app.listen(3000, () => {
