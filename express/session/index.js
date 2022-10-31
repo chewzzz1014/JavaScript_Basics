d0e42513a7deca3a71a2d6877e2d349e3d4a9e07
chewzzz <chewziqing@gmail.com>
2022-10-31 17:56:16 +0800

    Add express session

---
+++ express/session/index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const session = require("express-session");
+const app = express();
+
+const sessionOptions = {
+    secret: 'notagoodsecret',
+    resave: false,
+    saveUninitialized: false
+}
+
+// initialize express session and assign secret key
+app.use(session(sessionOptions))
+
+// every income req object has session property
+app.get("/viewcount", (req, res) => {
+    if (req.session.count) {
+        req.session.count += 1;
+    } else {
+        req.session.count = 1
+    }
+    res.send(`You've viewed this page ${req.session.count} times`);
+})
+
+
+app.get("/register", (req, res) => {
+    const { username = 'Anonymous' } = req.query;
+    req.session.username = username;
+    res.redirect("/greet");
+})
+
+app.get("/greet", (req, res) => {
+    const { username } = req.session;
+    res.send(`Welcome back, ${username}!!`)
+})
+
+
+app.listen(3000, () => {
+    console.log("Listening at Port 3000")
+})
