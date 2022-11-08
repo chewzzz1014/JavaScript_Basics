88ce23604d607089b690df44a1e068237b452fd2
chewzzz <chewziqing@gmail.com>
2022-11-08 12:01:08 +0800

    Add more examples

--- regex/regex9.js
+++ regex/regex9.js
@@ -5,3 +5,9 @@ wrongText.replace(silverRegex, 'blue');
 
 // capture group and replace them with group
 'Code Camp'.replace(/(\w+)\s(\w+)/, '$2 $1');
+
+
+// One Two Three => Three Two wrongTextlet str = "one two three";
+let fixRegex = /(\w+)\s(\w+)\s(\w+)/; // Change this line
+let replaceText = '$3 $2 $1'; // Change this line
+let result = str.replace(fixRegex, replaceText);
