3a50a5b6b3842cad2ba821176d58f51442c0bab9
chewzzz <chewziqing@gmail.com>
2022-11-10 22:54:23 +0800

    Add routes and register form ejs

--- express/Bcrypt_Auth/index.js
+++ express/Bcrypt_Auth/index.js
@@ -1,9 +1,21 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const User = require('./models/user')
+const path = require('path')
 const bcrypt = require('bcrypt')
 
 const app = express();
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+app.get('/register', (req, res) => {
+    res.render('register')
+})
+
+app.get('/secret', (req, res) => {
+    res.send('This is a secret!')
+})
 
 app.listen(3000, () => {
     console.log('Listening at Port 3000')
