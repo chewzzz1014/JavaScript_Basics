addb97ae04418208cb08d95c1ddfd66de76748be
chewzzz <chewziqing@gmail.com>
2022-11-10 22:48:12 +0800

    Add user model

--- express/Bcrypt_Auth/index.js
+++ express/Bcrypt_Auth/index.js
@@ -1,4 +1,10 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const User = require('./models/user')
 const bcrypt = require('bcrypt')
 
+const app = express();
+
+app.listen(3000, () => {
+    console.log('Listening at Port 3000')
+})
