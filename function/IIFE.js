2d837f1e91b55df9b850f108880bd8d126a83542
chewzzz <chewziqing@gmail.com>
2022-11-12 16:41:33 +0800

    Add example of using IIFE

--- function/IIFE.js
+++ function/IIFE.js
@@ -8,3 +8,25 @@
 (function(){
   console.log('A cozy nest is ready')
 }())
+
+
+
+// create module using iife
+// group related functionality into single module 
+let isCuteMixin = function(obj) {
+  obj.isCute = function() {
+    return true;
+  };
+};
+let singMixin = function(obj) {
+  obj.sing = function() {
+    console.log("Singing to an awesome tune");
+  };
+};
+
+const funModule = (function(){
+      return{
+          isCuteMixin: isCuteMixin,
+          singMixin: singMixin
+      } // end of return stmt
+})()
