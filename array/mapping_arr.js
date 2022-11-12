e8a984ff07f47cba0046eaaff487d2d70fbb6126
chewzzz <chewziqing@gmail.com>
2022-11-12 17:30:40 +0800

    Add mapping using forEach; move watchList into new file

--- array/mapping_arr.js
+++ array/mapping_arr.js
@@ -1,4 +1,4 @@
-import {watchList} from './mapping_arr_data'
+let watchList = require('./mapping_arr_data')
 
 const ratings = watchList.map((ele)=>{
   return {
@@ -8,3 +8,19 @@ const ratings = watchList.map((ele)=>{
 })
 
 console.log(JSON.stringify(ratings));
+
+
+
+// mapping using forEach and callback
+// The global variable
+const s = [23, 65, 98, 5];
+
+Array.prototype.myMap = function(callback) {
+  const newArray = [];
+  this.forEach((ele)=> newArray.push(callback(ele)))
+  return newArray;
+};
+
+const new_s = s.myMap(function(item) {
+  return item * 2;
+});
