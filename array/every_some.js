0702fce68afae174fe0eb95ad375f4a408056bbf
chewzzz <chewziqing@gmail.com>
2022-11-12 19:00:14 +0800

    Add some array method

--- array/every_some.js
+++ array/every_some.js
@@ -4,3 +4,12 @@ function checkPositive(arr) {
 }
 
 checkPositive([1, 2, 3, -4, 5]);
+
+
+// some: works with arrays to check if any element passes a particular test. 
+// It returns a Boolean value - true if any of the values meet the criteria, false if not.
+function checkPositive(arr) {
+  return arr.some((ele)=> ele>0)
+}
+
+checkPositive([1, 2, 3, -4, 5]);
