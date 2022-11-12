748d8c24d245644bc4c07ac84dffda79bbd289ba
chewzzz <chewziqing@gmail.com>
2022-11-12 16:32:55 +0800

    Add IIFE.js

---
+++ function/IIFE.js
@@ -0,0 +1,10 @@
+// immediate invoked function expression
+// execute a function as soon as it's declared
+// anonymous function
+(function(){
+  console.log('Hi, IIFE!')
+}())
+
+(function(){
+  console.log('A cozy nest is ready')
+}())
