0e8d69bb7e35db27d7cad80a72c74af3e73128a3
chewzzz <chewziqing@gmail.com>
2022-11-12 18:20:04 +0800

    More example

--- regex/split_sentence.js
+++ regex/split_sentence.js
@@ -6,3 +6,11 @@ function splitify(str) {
 splitify("Hello World,I-am code");
 splitify("Earth-is-our home")
 splitify("This.is.a-sentence")
+
+
+// replace non-word with spaces
+function sentensify(str) {
+  return str.split(/\W/).join(" ")
+}
+
+sentensify("May-the-force-be-with-you");
