14179788a0d91044b542ad39214da809ff45891c
chewzzz <chewziqing@gmail.com>
2022-11-12 18:06:09 +0800

    Add finding squares of positive integers

--- array/mapping_filtering.js
+++ array/mapping_filtering.js
@@ -9,3 +9,16 @@ const filteredList = watchList.filter((ele)=> Number(ele.imdbRating) > 8).map((e
 
 
 console.log(filteredList);
+
+
+
+// square of positive integer
+const squareList = arr => {
+  return arr
+          .filter((ele)=> ele>0 && Number.isInteger(ele))
+          .map((ele)=> ele**2)
+
+};
+
+const squaredIntegers = squareList([-3, 4.8, 5, 3, -3.2]);
+console.log(squaredIntegers);
