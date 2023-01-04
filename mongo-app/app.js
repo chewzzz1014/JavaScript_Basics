49666691ad7f65ebb4a0d1e412506f9554dabcab
chewzzz <chewziqing@gmail.com>
2023-01-04 21:25:22 +0800

    Add GET request handler for a single document

--- mongo-app/app.js
+++ mongo-app/app.js
@@ -1,3 +1,4 @@
+const { ObjectID } = require('mongodb')
 const express = require('express')
 const { connectToDb, getDb } = require('./db')
 const app = express()
@@ -27,6 +28,16 @@ app.get('/books', async (req, res) => {
         res.status(500).json({
             error
         })
+    }
+})
+
+app.get('/books/:id', async (req, res) => {
+    const { id } = req.params
+    try {
+        const foundBook = await db.collection('books').findOne({ _id: ObjectID(id) })
+        res.status(200).json({ foundBook })
+    } catch (error) {
+        res.status(500).json({ error })
     }
 })
 
