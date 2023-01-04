b38e97f583607ed5531a7f092492164fc6ce1a27
chewzzz <chewziqing@gmail.com>
2023-01-04 21:06:15 +0800

    Fetch documents from database

--- mongo-app/app.js
+++ mongo-app/app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const { connectToDb, getDb } = require('./db')
 const app = express()
 
+// database returned from connection
 let db
 
 // connect to db
@@ -11,10 +12,22 @@ connectToDb((e) => {
     }
 })
 
-app.get('/books', (req, res) => {
-    res.json({
-        msg: 'welcome to api'
-    })
+app.get('/books', async (req, res) => {
+    let allBooks = []
+    try {
+        await db.collection('books')
+            .find()  // return cursor 
+            .sort({ author: 1 })
+            .forEach(b => allBooks.push(b))
+
+        res.status(200).json({
+            allBooks
+        })
+    } catch (error) {
+        res.status(500).json({
+            error
+        })
+    }
 })
 
 app.listen(3000, () => {
