8432b428a507d55bec8576a0f2d7c5be80ec3af6
chewzzz <chewziqing@gmail.com>
2023-02-18 16:02:50 +0800

    done q4-q10

--- exercises/q1-q10.js
+++ exercises/q1-q10.js
@@ -51,4 +51,52 @@ const inviteE1 = invite.bind(employee1)
 const inviteE2 = invite.bind(employee2)
 
 
+// Q4: What's JSON and its common operations?
+JSON.parse('{"name": "chewzzz"}') // JSON -> JS Object
+JSON.stringify(obj4) // object -> JSON
 
+
+// Q5: Purpose of array's slice method?
+// create copy/substring of array
+// arr.slice(startIdx, [endIdx (exclusive)])
+const arr = [1, 2, 3, 4, 5]
+console.log(arr.slice(2, 4)) // [3, 4]
+
+// Q6: Purpose of array's splice method?
+// delete and insert elements at once (will mutate the original array). Return deleted array
+// arr.splice(startIdx, [numEle to delete], [element inserted], [element inserted]...)
+console.log(arr.splice(0, 0, 0.5, 0.6, 0.7)) // []
+
+
+// Q7: Diff between slice and splice
+// slice is immutable; splice is mutable
+// slice returns subset of array; splice return an array of elements deleted
+
+
+// Q8: How to compare Object and Map?
+/**
+ * 1. The key for Object can only be string or symbol. For key of map, it can be anything.
+ *
+ * 2. Keys in Map are ordered. Object are not
+ *
+ * 3. Can get size of map using size() method. For object, need to count manually.
+ *
+ * 4. Map is iterable
+ *
+ * 5. Object has prototype.
+ *
+ * 6. Map performs better for frequent addition and removal of key pairs
+ */
+
+
+// Q9: Diff bet. == and ===
+// == compares by value and can be diff data types
+// === only compares values of same types
+// 0 === -1
+// NaN !=== NaN
+// null !== undefined; null == undefined
+
+
+// Q10: What're lambda or arrow functions
+// shorted syntax of function expression
+// doesnt have `this`, `arguments`, `super`, `new.target`
