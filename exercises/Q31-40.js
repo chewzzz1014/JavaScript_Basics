fa1e1af868457282b75cbfb2679e40b35d2e2195
chewzzz <chewziqing@gmail.com>
2023-02-23 15:31:28 +0800

    Add Q32

--- exercises/Q31-40.js
+++ exercises/Q31-40.js
@@ -1,3 +1,6 @@
 // Q31: Service Worker
 // a script that runs in bg, seperate from web page and provides features that dont need web page/ui
 
+// Q32: How service worker manipulate DOM?
+// they cant access directly, but they communicate w/ pages they control by responding to msg sent via postMessageinterface
+
