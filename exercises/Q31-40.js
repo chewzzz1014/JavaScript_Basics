b4f3c7523bb157cbb1a7fcad5c20df411e3ea87a
chewzzz <chewziqing@gmail.com>
2023-02-23 15:32:31 +0800

    Add Q33

--- exercises/Q31-40.js
+++ exercises/Q31-40.js
@@ -4,3 +4,8 @@
 // Q32: How service worker manipulate DOM?
 // they cant access directly, but they communicate w/ pages they control by responding to msg sent via postMessageinterface
 
+// Q33: How to reuse information across service worker restarts
+// The problem with service worker is that it gets terminated when not in use, and restarted when
+// it's next needed, so you cannot rely on global state within a service worker's onfetch and
+// onmessage handlers.In this case, service workers will have access to IndexedDB API in order to
+// persist and reuse across restarts
