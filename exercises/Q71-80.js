688ef7a8c6a55a62fec6d8c5546a7f0f89d38f77
chewzzz <chewziqing@gmail.com>
2023-03-04 17:56:18 +0800

    Add Q71-Q75

---
+++ exercises/Q71-80.js
@@ -0,0 +1,30 @@
+// Q71: typeof
+// to determine type of varible. Return the type in string
+
+
+// Q72, Q73, Q74: null VS undefined
+/**
+ * null:
+ *  - variable point to no object
+ *  - type: object
+ *  - primitive value
+ *  - indicate absence of value for a variable
+ *  - converted to 0 in primitive operations
+ *
+ * undefined:
+ *  - variable has been declared but not yet assigned a value
+ *  - type: undefined
+ *  - primitive value
+ *  - indicate absence of variable itself
+ *  - converted to NaN while in primitive operations
+ *
+ */
+
+
+// Q75: eval
+// evaluate JS code represented as code
+console.log(eval('1 + 2')) // 3
+
+
+// 
+
