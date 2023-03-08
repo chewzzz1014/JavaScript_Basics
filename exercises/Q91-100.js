315d257cea8eb8d95f0b32ee9ecdb4d00b03c74d
chewzzz <chewziqing@gmail.com>
2023-03-08 17:40:43 +0800

    Add Q91-92

---
+++ exercises/Q91-100.js
@@ -0,0 +1,11 @@
+// Q91: Diff between native object, host object, user object
+// native: objects that're part of ECMAScript specification. Eg, string, math, regexp
+// host: objects provided by browser/runtime environment (Node). Eg, window, XmlHttpRequest, DOM nodes
+// user: objects defined in JS code
+
+
+// Q92: Tools/Technique used for debugging JS Code
+// chrome devtools, debugger stmt, console.log
+
+
+// Q93: Promises compare to callback
