318b9f5b6ce0e84fa5b922e751d10bd0c1e07f21
chewzzz <chewziqing@gmail.com>
2023-04-14 21:41:22 +0800

    Add Q113-115

--- exercises/Q111-Q220.js
+++ exercises/Q111-Q220.js
@@ -9,3 +9,24 @@
     3. {} hold objects
     4. [] hold arrays
 */
+
+
+// Q113: Purpose of JSON stringify
+// object/array => JSON
+const userJSON = {'name': 'John', age: 31}
+const userString = JSON.stringify(user)
+console.log(userString)
+
+
+// Q114: Parse JSON string
+// string => object/array
+const userString1 = '{"name": "John", "age": 31}'
+const userJSON1 = JSON.parse(userString1)
+console.log(userJSON1)
+
+
+// Q115: Why need JSON
+// exchange data between browser and server
+// sent to and from a server 
+
+
