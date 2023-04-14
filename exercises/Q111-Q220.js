a2da38003efe39972a9f63a6c423b1fee9afd956
chewzzz <chewziqing@gmail.com>
2023-04-14 21:36:14 +0800

    Add Q11-112

---
+++ exercises/Q111-Q220.js
@@ -0,0 +1,11 @@
+// Q111: What's JSON
+// lightweight format used for data interchanging
+
+
+// Q112: Syntax rules of JSON
+/*
+    1. Data in name/value pairs
+    2/ Separated by commas
+    3. {} hold objects
+    4. [] hold arrays
+*/
