fd096c3f497ff3b8c4b7db1e2908c73c8939a84a
chewzzz <chewziqing@gmail.com>
2023-06-26 16:10:41 +0800

    Q130

--- exercises/Q121-130.js
+++ exercises/Q121-130.js
@@ -92,3 +92,12 @@ function titleCase(str) {
     return str[0].toUpperCase() + str.slice(1)
 }
 titleCase('hello')
+
+
+// Q130: current date
+var today = new Date();
+var dd = String(today.getDate()).padStart(2, '0');
+var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+var yyyy = today.getFullYear();
+today = mm + '/' + dd + '/' + yyyy;
+document.write(today);
