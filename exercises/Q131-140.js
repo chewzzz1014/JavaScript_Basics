8fa61f1f9b023b7fece4942799bee0c51d5303fc
chewzzz <chewziqing@gmail.com>
2023-06-29 11:46:58 +0800

    Q132

--- exercises/Q131-140.js
+++ exercises/Q131-140.js
@@ -5,3 +5,9 @@ var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
 var yyyy = today.getFullYear();
 today = mm + '/' + dd + '/' + yyyy;
 document.write(today);
+
+// Q132: compare 2 data object
+var d1 = new Date();
+var d2 = new Date(d1);
+console.log(d1.getTime() === d2.getTime()); //True
+console.log(d1 === d2); // False
