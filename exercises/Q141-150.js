50b5f6071fd82801fcb80b695ab27d7c825760a6
chewzzz <chewziqing@gmail.com>
2023-07-10 14:00:30 +0800

    141

---
+++ exercises/Q141-150.js
@@ -0,0 +1,6 @@
+// Q141: number of parameters for a function
+// function.length
+function sum(num1, num2, num3, num4) {
+    return num1 + num2 + num3 + num4;
+}
+sum.length // 4 is the number of parameters expected.
