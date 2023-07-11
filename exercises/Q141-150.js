d0f7146a0e5d2e6a42962264d117ca4f446f8cc9
chewzzz <chewziqing@gmail.com>
2023-07-11 18:57:20 +0800

    142

--- exercises/Q141-150.js
+++ exercises/Q141-150.js
@@ -4,3 +4,11 @@ function sum(num1, num2, num3, num4) {
     return num1 + num2 + num3 + num4;
 }
 sum.length // 4 is the number of parameters expected.
+
+
+// Q142: polyfill
+/*
+provide modern functionality on older browsers that do
+not natively support it. For example, Silverlight plugin polyfill can be used to mimic the
+functionality of an HTML Canvas element on Microsoft Internet Explorer 7
+*/
